4e368ed12ecd0d8fd54fe1b7f787c5f73c736ffc
Kelvin Yuri <kelvin.yuri@kysoftware.com.br>
2021-08-22 16:58:28 -0300

    Trazendo a versão final de outro repositorio

---
+++ App.js
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+
+
+// or any pure javascript modules available in npm
+import { TextInput, Button } from 'react-native-paper';
+
+export default class App extends React.Component {
+
+  state = {
+      peso: 0,
+      altura: 0,
+      imc: 0,
+      legenda: 'Indeterminado',
+      cor: '#bdc3c7',
+  };
+
+  calcularIMC =() => {
+    const resultado = this.state.peso / (this.state.altura * this.state.altura);
+
+    this.setState({
+      imc: Math.ceil(resultado)
+    });
+
+    if(resultado < 18.5){
+      this.setState({
+        legenda: 'Magreza',
+        cor: '#e74c3c',
+      });
+    } else if(resultado >= 18.5 && resultado < 25){
+      this.setState({
+        legenda: 'Normal',
+        cor: '#2ecc71',
+      });
+    } else if(resultado >= 25 && resultado < 30){
+      this.setState({
+        legenda: 'Sobrepeso',
+        cor: '#f1c40f',
+      });
+    } else if(resultado >= 30 && resultado <40){
+      this.setState({
+        legenda: 'Obesidade',
+        cor: '#e67e22',
+      });
+    }else if(resultado > 40){
+      this.setState({
+        legenda: 'Obesidade grave',
+        cor: '#e74c3c',
+      });
+    }
+
+  };
+
+  render() {
+    return (
+      <View style={styles.app}>
+        <Text style={styles.legenda}>Seu IMC</Text>
+
+        <View style={[styles.painel, {backgroundColor: this.state.cor}]}>
+          <Text style={styles.resultado}>{this.state.imc}</Text>
+          <Text style={styles.diagnostico}>{this.state.legenda}</Text> 
+        </View>
+
+        <View>
+          <TextInput
+            label= 'Peso' 
+            style={styles.peso}
+            onChangeText={valor => {
+              this.setState({peso: valor.replace(',', '.')});
+            }} 
+          />
+
+          <TextInput
+            label = 'Altura'
+            style={styles.altura}
+            onChangeText={valor => {
+              this.setState({altura: valor.replace(',', '.')});
+            }} 
+          />
+          <Button onPress={this.calcularIMC} mode = 'contained'>
+            Calcular
+          </Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  app: {
+      padding: 10,
+
+  },
+  legenda: {
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  resultado: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+  diagnostico: {
+    textAlign: "center",
+    fontSize: 16,
+  },
+  peso: {
+    marginVertical: 10,
+
+  },
+  altura:{
+    marginVertical: 10,
+  },
+  painel: {
+    borderRadius: 5,
+    marginVertical: 10,
+    padding: 8,
+    width: 150,
+    alignSelf: "center",
+  },
+});
